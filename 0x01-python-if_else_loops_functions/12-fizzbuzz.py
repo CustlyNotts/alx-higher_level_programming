67ab8fed21b8c9ac4bb00f63ca165526cd80e9dc
CustlyNotts <kenrovers95@gmail.com>
2022-06-01 02:01:40 +0000

    Complete FizzBuzz

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def fizzbuzz():
+    for i in range (1, 101):
+        if i % 3 == 0:
+            i = 'Fizz'
+        elif i % 5 == 0:
+            i = 'Buzz'
+        elif i % 15 == 0:
+            i = 'FizzBuzz'
+        print('{}'.format(i), end=" ")
